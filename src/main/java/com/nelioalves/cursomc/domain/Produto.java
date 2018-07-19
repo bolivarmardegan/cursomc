e2e969ca54a86a79ebdc0052b9d81ca7733ec779
bolivar mardegan dos santos <bolivar.mardegan@unicid.edu.br>
2018-07-19 10:43:37 -0300

    EndPoint Pedidos{id} -- Disponibilizado

--- src/main/java/com/nelioalves/cursomc/domain/Produto.java
+++ src/main/java/com/nelioalves/cursomc/domain/Produto.java
@@ -17,6 +17,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Produto implements Serializable {
@@ -39,7 +40,7 @@ public class Produto implements Serializable {
 				inverseJoinColumns= @JoinColumn(name="categoria_id")
 	)
 	private List<Categoria> categorias = new ArrayList<>();
-	
+	@JsonIgnore
 	@OneToMany(mappedBy="id.produto")
 	private Set<ItemPedido> itens = new HashSet<>();
 	
@@ -53,6 +54,7 @@ public class Produto implements Serializable {
 		this.preco = preco;
 	}
 	
+	@JsonIgnore
 	public List<Pedido> getPedidos(){
 		ArrayList<Pedido> lista = new ArrayList<>();
 		for (ItemPedido pedido : itens) {
