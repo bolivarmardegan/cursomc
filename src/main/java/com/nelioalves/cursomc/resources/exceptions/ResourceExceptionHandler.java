7ff05546468197de9e452066fa275975390e5ffb
bolivar mardegan dos santos <bolivar.mardegan@unicid.edu.br>
2018-07-24 10:52:05 -0300

    Validação no PUT -- CategoriaDTO

--- src/main/java/com/nelioalves/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/nelioalves/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -2,6 +2,8 @@ package com.nelioalves.cursomc.resources.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -24,6 +26,16 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(DataIntegretyException.class)
 	public ResponseEntity<StandardError> dataIntegrety(DataIntegretyException e,HttpServletRequest request){
 		StandardError err =  new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Não é possível excluir uma categoria que possui Produtos.", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+		
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e,HttpServletRequest request){
+		ValidationError err =  new ValidationError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Erro de validação.", e.getMessage(), request.getRequestURI());
+		for (FieldError error : e.getBindingResult().getFieldErrors()) {
+			err.addError(error.getField(), error.getDefaultMessage());
+		}
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 		
 	}
