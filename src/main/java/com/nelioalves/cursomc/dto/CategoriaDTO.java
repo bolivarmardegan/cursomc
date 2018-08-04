7ab5730f4a5df1f29cc1ecf8a6bad1c1fc4a60b0
Bolívar Mardegan dos Santos <bolivar.android@gmail.com>
2018-08-03 21:08:17 -0300

    Update

--- src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
@@ -13,7 +13,7 @@ public class CategoriaDTO  implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private Integer id;
 	@NotEmpty(message="Preenchimento obrigatório")
-	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
+	@Length(min=5, max=100, message="O tamanho deve ser entre 5 e 100 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
